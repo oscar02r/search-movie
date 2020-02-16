7085f96f66d0a371409dca8b9b3ae1e464d43d28
oscar02r <oscary.ramirez@gmail.com>
2020-02-16 20:42:20 +0100

    Added titlecomponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Title} from './components/Title'
 
+import 'bulma/css/bulma.css'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-     
+    <Title>Search Movies</Title> 
     </div>
   );
 }
