594b599d504de39471c5cfebb519c2e14eca88f6
oscar02r <oscary.ramirez@gmail.com>
2020-02-17 01:17:38 +0100

    Added component list movies on two colum

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react'
 import PropTypes from 'prop-types'
-import {Movie} from './Movie'
+import { Movie } from './Movie'
 
 export class MovieList extends Component{
   static propTypes = {
@@ -9,16 +9,25 @@ export class MovieList extends Component{
 
   render(){
      const { movies } = this.props
-
-      return movies.map(movie =>{
-        return(
-           <Movie
-             key={movie.imdbID}
-             title={movie.Title}
-             year={movie.Year}
-             poster={movie.Poster}
-           />
+       
+     return(
+         <div className='MovieList'>
+          {
+              movies.map(movie =>{
+              return(
+                  <div key={movie.imdbID} className='MoviesList-item'>
+                <Movie
+                  title={movie.Title}
+                  year={movie.Year}
+                  poster={movie.Poster}
+                />
+                </div>
            )
-  }
+       })
+          }
+         </div>
+     )
+       
 
+   }
 }
